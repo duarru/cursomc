e922a8ac85a191c5a09534733e6da33a3c77e943
Obedis Duarte <duarru31@gmail.com>
2024-07-13 18:11:24 -0300

    Estado e Cidade.

---
+++ src/main/java/com/nelioalves/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,13 @@
+// acessar os dados dos objetos do tipo Categoria.
+
+package com.nelioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nelioalves.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+}
