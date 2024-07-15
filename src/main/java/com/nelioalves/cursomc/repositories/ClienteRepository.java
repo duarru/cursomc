293abefc4821efc3d0cee4ecc78300fefaa04b5f
Obedis Duarte <duarru31@gmail.com>
2024-07-14 21:20:07 -0300

    Cliente TipoCliente, telefones e endereços.

---
+++ src/main/java/com/nelioalves/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,13 @@
+// acessar os dados dos objetos do tipo Categoria.
+
+package com.nelioalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nelioalves.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+}
